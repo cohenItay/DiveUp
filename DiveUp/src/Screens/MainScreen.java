6d139799eb221eb7c84a446e49780fbf8e9722a4
Maor <maorlolz1@gmail.com>
2019-05-14 23:35:57 +0300

    Added DB file , and sqlConnection class

---
+++ DiveUp/src/Screens/MainScreen.java
@@ -0,0 +1,50 @@
+package Screens;
+
+import java.awt.EventQueue;
+import java.sql.Connection;
+
+import javax.swing.JFrame;
+
+import org.sqlite.core.DB;
+
+public class MainScreen {
+
+	private JFrame frame;
+	public static sqlConnection dbConnection;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					dbConnection = new sqlConnection();
+					Connection conn = dbConnection.Connect();
+					dbConnection.runQuery(conn, "SELECT * from DIVER");
+					MainScreen window = new MainScreen();
+					window.frame.setVisible(true);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
