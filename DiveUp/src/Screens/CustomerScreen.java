70f10c0dfef0bb3381bd9a22fa8e9d423bc0d51d
Konfino <Maor.Konfino@Honeywell.com>
2019-05-16 22:56:56 +0300

    Started building customer screen, added a little design

--- DiveUp/src/Screens/CustomerScreen.java
+++ DiveUp/src/Screens/CustomerScreen.java
@@ -1,7 +1,11 @@
 package Screens;
 
+import java.awt.CardLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Image;
+import java.awt.LayoutManager;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -14,13 +18,16 @@ import javax.swing.table.DefaultTableModel;
 import org.sqlite.core.DB;
 
 import Classes.Diver;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class CustomerScreen {
 
 	private JFrame frame;
-	private JTable diversTable;
 	private DefaultTableModel model; 
 	private List<Diver> diversList;
+	private JButton button;
 	/**
 	 * Launch the application.
 	 */
@@ -68,14 +75,21 @@ public class CustomerScreen {
 		frame.setBounds(100, 100, 600, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
 		String[] colHeadings = {"ID","First Name","Last Name","License ID","Email","Phone"};
 		int numRows = 0 ;
 		model = new DefaultTableModel(numRows, colHeadings.length) ;
 		model.setColumnIdentifiers(colHeadings);
-		diversTable = new JTable(model);		
-		JScrollPane scroll = new JScrollPane(diversTable);
+		frame.getContentPane().setLayout(new MigLayout("", "[407.00][][74.00][244.00][570px]", "[210.00][134.00][][][123.00][][][418px]"));
+		JTable diversTable = new JTable(model);
+		JScrollPane scrollPane = new JScrollPane(diversTable);
+		frame.getContentPane().add(scrollPane, "cell 0 0 5 1,growx,aligny center");
+		
+		JButton addDiverButton = new JButton("\u05D4\u05D5\u05E1\u05E4\u05EA \u05E6\u05D5\u05DC\u05DC\u05DF");
+		frame.getContentPane().add(addDiverButton, "cell 4 1");
+		
+		JButton divingBookButton = new JButton("\u05D4\u05E6\u05D2 \u05D9\u05D5\u05DE\u05DF \u05E6\u05DC\u05D9\u05DC\u05D4");
+		frame.getContentPane().add(divingBookButton, "cell 3 1");
+		
 		updateDiversTable();
-		frame.getContentPane().add(scroll);
 	}
 }
