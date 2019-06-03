14b5ad68f733fb1bb26b8fe68f0a65ad45f3c1e0
MaorKo <maorlolz1@gmail.com>
2019-06-03 18:53:40 +0300

    Prevent closing sale screen added

--- DiveUp/src/Screens/SaleScreen.java
+++ DiveUp/src/Screens/SaleScreen.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.text.DateFormat;
@@ -180,7 +181,7 @@ public class SaleScreen {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(UIConstants.miniScreenx, UIConstants.miniScreeny, UIConstants.miniScreenWidth,UIConstants.miniScreenHeight);
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		//Title and icon add
 		frame.setTitle("Sale screen");
 		Image image;
@@ -400,6 +401,19 @@ public class SaleScreen {
 		itemsTable.clearSelection();
 		frame.setVisible(true);
 		
+		frame.addWindowListener(new WindowAdapter() {
+	         @Override
+	         public void windowClosing(WindowEvent e) {
+	            if(cartTable.getRowCount() > 0)
+	            {
+	            	errorMessage("לא ניתן לסגור את החלון לפני הסרת כל המוצרים מהעגלה", "שגיאה");
+	            }
+	            else
+	            {
+	            	frame.dispose();
+	            }
+	         }
+	      });
 	}
 
 }
